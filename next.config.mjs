b6c5a7110dbf63ff1bab0e014fc8935e1ebc7625
GiFTED <gkenny896@gmail.com>
2024-04-16 13:31:02 +0100

    setup & create todo list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
+        protocol: "http",
+      },
+      {
+        hostname: "*",
+        protocol: "https",
+      },
+    ],
+  },
+  reactStrictMode: false,
+  env: {
+    WC_PROJECT_ID: process.env.WC_PROJECT_ID,
+    PEANUT_API_KEY: process.env.PEANUT_API_KEY,
+  },
+};
 
 export default nextConfig;
